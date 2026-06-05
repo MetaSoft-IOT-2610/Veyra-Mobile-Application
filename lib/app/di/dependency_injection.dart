1c677d0857485ff7653799e65d9accede0c1ccb3
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-06-05 18:48:28 -0500

    feat(shared): implement dynamic base URL for HTTP client based on platform

--- lib/app/di/dependency_injection.dart
+++ lib/app/di/dependency_injection.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart'; // Importante para kIsWeb y defaultTargetPlatform
 import 'package:get_it/get_it.dart';
 import '../../shared/application/contracts/i_http_client.dart';
 import '../../shared/infrastructure/network/dio_http_client_impl.dart';
@@ -22,9 +23,20 @@ Future<void> initDependencies() async {
 }
 
 void _initSharedKernel() {
+  // Función para determinar la URL base según la plataforma actual
+  String getBaseUrl() {
+    if (kIsWeb) {
+      return 'http://localhost:8080/api/v1/'; // Navegadores web (Chrome, Edge, Safari)
+    } else if (defaultTargetPlatform == TargetPlatform.android) {
+      return 'http://10.0.2.2:8080/api/v1/';  // Emuladores Android
+    } else {
+      return 'http://localhost:8080/api/v1/'; // Simuladores iOS o Desktop
+    }
+  }
+
   // Registrar el cliente HTTP corporativo apuntando a tu backend de Spring Boot
   locator.registerLazySingleton<IHttpClient>(
-    () => DioHttpClientImpl('http://localhost:8080/api/v1/'), 
+    () => DioHttpClientImpl(getBaseUrl()), // Pasamos la URL dinámica
   );
   
   // Aquí en el futuro registrarás ILocalStorage, IConnectivityService, etc.
