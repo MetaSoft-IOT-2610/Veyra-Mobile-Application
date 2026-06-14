a783ff4a535c92ce533206e6fd1cd3d16fe6a7d6
Renxoll <112541849+Renxoll@users.noreply.github.com>
2026-06-14 16:26:56 -0500

    feat(nursing): update exports in nursing.dart to include resident_directory_page and maintain resident_summary_widget

--- lib/nursing/nursing.dart
+++ lib/nursing/nursing.dart
@@ -3,6 +3,7 @@
 // 1. Exportamos el inicializador de inyección de dependencias
 export 'nursing_module.dart';
 
-// 2. Exportamos las páginas y widgets públicos (como el Dashboard)
+// 2. Exportamos las páginas y widgets públicos
 export 'presentation/pages/admin_dashboard_page.dart';
+export 'presentation/pages/resident_directory_page.dart';
 export 'presentation/widgets/resident_summary_widget.dart';
