85614f059ea7eadc9221e4280c5c63631cb774ee
RenzoVi21 <renzovillata@gmail.com>
2026-06-20 15:26:14 -0500

    feat(nursing): wire resident module dependencies

--- lib/nursing/nursing.dart
+++ lib/nursing/nursing.dart
@@ -6,4 +6,6 @@ export 'nursing_module.dart';
 // 2. Exportamos las páginas y widgets públicos
 export 'presentation/pages/admin_dashboard_page.dart';
 export 'presentation/pages/resident_directory_page.dart';
+export 'presentation/pages/create_resident_page.dart';
+export 'presentation/pages/resident_detail_page.dart';
 export 'presentation/widgets/resident_summary_widget.dart';
--- lib/nursing/nursing_module.dart
+++ lib/nursing/nursing_module.dart
@@ -6,6 +6,7 @@ import 'infrastructure/datasources/nursing_remote_datasource.dart';
 import 'infrastructure/repositories/nursing_home_setup_repository_impl.dart';
 import 'infrastructure/repositories/nursing_repository_impl.dart';
 import 'application/commands/create_nursing_home_command.dart';
+import 'application/commands/create_resident_command.dart';
 import 'application/queries/get_resident_summary_query.dart';
 import 'application/queries/get_resident_list_query.dart';
 import 'presentation/bloc/nursing_home_setup_bloc.dart';
@@ -32,11 +33,12 @@ void initNursingModule(GetIt locator) {
   locator.registerLazySingleton(() => GetResidentSummaryQuery(locator()));
   locator.registerLazySingleton(() => GetResidentListQuery(locator()));
   locator.registerLazySingleton(() => CreateNursingHomeCommand(locator()));
+  locator.registerLazySingleton(() => CreateResidentCommand(locator()));
 
   // 4. BLoCs (Siempre como Factory)
   locator.registerFactory(
     // Ahora pasamos los dos casos de uso al BLoC
-    () => NursingBloc(locator(), locator()),
+    () => NursingBloc(locator(), locator(), locator(), locator()),
   );
   locator.registerFactory(() => NursingHomeSetupBloc(locator()));
 }
