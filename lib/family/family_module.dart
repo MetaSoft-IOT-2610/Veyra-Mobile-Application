c3d1385737c8127c2566fe0395e0594bf1859c52
RenzoVi21 <renzovillata@gmail.com>
2026-06-21 03:24:50 -0500

    feat(app): register family portal module

---
+++ lib/family/family_module.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+
+import 'application/queries/get_family_portal_query.dart';
+import 'domain/repositories/i_family_portal_repository.dart';
+import 'infrastructure/datasources/family_portal_remote_datasource.dart';
+import 'infrastructure/repositories/family_portal_repository_impl.dart';
+import 'presentation/bloc/family_portal_bloc.dart';
+
+void initFamilyModule(GetIt locator) {
+  locator.registerLazySingleton<FamilyPortalRemoteDataSource>(
+    () => FamilyPortalRemoteDataSourceImpl(client: locator()),
+  );
+  locator.registerLazySingleton<IFamilyPortalRepository>(
+    () => FamilyPortalRepositoryImpl(remoteDataSource: locator()),
+  );
+  locator.registerLazySingleton(() => GetFamilyPortalQuery(locator()));
+  locator.registerFactory(() => FamilyPortalBloc(locator()));
+}
