c0a6d75c923abf3c7b37a105655ffcfeac3a08a7
RenzoVi21 <renzovillata@gmail.com>
2026-06-21 16:17:20 -0500

    refactor(iam): keep sign-in state only

--- lib/iam/presentation/bloc/auth_bloc.dart
+++ lib/iam/presentation/bloc/auth_bloc.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../application/commands/login_command.dart';
-import '../../application/commands/sign_up_command.dart';
 import '../../domain/entities/auth_session.dart';
 
 /// Events processed by the [AuthBloc].
@@ -13,45 +12,12 @@ class PerformLoginEvent extends AuthEvent {
   PerformLoginEvent(this.username, this.password);
 }
 
-/// Event triggered when the user creates a new account.
-class PerformSignUpEvent extends AuthEvent {
-  final String username;
-  final String password;
-  final String confirmPassword;
-
-  PerformSignUpEvent({
-    required this.username,
-    required this.password,
-    required this.confirmPassword,
-  });
-}
-
-class PerformAdministratorSignUpEvent extends AuthEvent {
-  final String username;
-  final String password;
-  final String confirmPassword;
-
-  PerformAdministratorSignUpEvent({
-    required this.username,
-    required this.password,
-    required this.confirmPassword,
-  });
-}
-
 /// States emitted by the [AuthBloc].
 abstract class AuthState {}
 
 class AuthInitial extends AuthState {}
 
 class AuthLoading extends AuthState {}
-
-class AuthSignUpSuccess extends AuthState {}
-
-class AuthAdministratorSignUpSuccess extends AuthState {
-  final int administratorId;
-
-  AuthAdministratorSignUpSuccess(this.administratorId);
-}
 
 /// State emitted when authentication is successful.
 class AuthSuccess extends AuthState {
@@ -68,9 +34,8 @@ class AuthError extends AuthState {
 /// Business Logic Component managing the Authentication state.
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final LoginCommand _loginCommand;
-  final SignUpCommand _signUpCommand;
 
-  AuthBloc(this._loginCommand, this._signUpCommand) : super(AuthInitial()) {
+  AuthBloc(this._loginCommand) : super(AuthInitial()) {
     on<PerformLoginEvent>((event, emit) async {
       emit(AuthLoading());
 
@@ -85,35 +50,5 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       );
     });
 
-    on<PerformSignUpEvent>((event, emit) async {
-      emit(AuthLoading());
-
-      final result = await _signUpCommand.execute(
-        username: event.username,
-        password: event.password,
-        confirmPassword: event.confirmPassword,
-      );
-
-      result.fold(
-        (failure) => emit(AuthError(failure.message)),
-        (_) => emit(AuthSignUpSuccess()),
-      );
-    });
-
-    on<PerformAdministratorSignUpEvent>((event, emit) async {
-      emit(AuthLoading());
-
-      final result = await _signUpCommand.executeAdministrator(
-        username: event.username,
-        password: event.password,
-        confirmPassword: event.confirmPassword,
-      );
-
-      result.fold(
-        (failure) => emit(AuthError(failure.message)),
-        (administratorId) =>
-            emit(AuthAdministratorSignUpSuccess(administratorId)),
-      );
-    });
   }
 }
