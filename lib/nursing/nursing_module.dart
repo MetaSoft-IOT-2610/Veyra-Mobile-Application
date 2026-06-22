9c2b4417c4813c4bf2d149e1362de3f5dd64ec29
RenzoVi21 <renzovillata@gmail.com>
2026-06-21 22:25:22 -0500

    refactor(nursing): remove mobile creation workflows

--- lib/nursing/nursing_module.dart
+++ lib/nursing/nursing_module.dart
@@ -1,18 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'domain/repositories/i_nursing_repository.dart';
-import 'domain/repositories/i_nursing_home_setup_repository.dart';
-import 'infrastructure/datasources/nursing_home_setup_remote_datasource.dart';
 import 'infrastructure/datasources/nursing_remote_datasource.dart';
-import 'infrastructure/repositories/nursing_home_setup_repository_impl.dart';
 import 'infrastructure/repositories/nursing_repository_impl.dart';
-import 'application/commands/create_nursing_home_command.dart';
 import 'application/commands/create_relative_command.dart';
 import 'application/commands/create_resident_command.dart';
 import 'application/queries/get_family_users_query.dart';
 import 'application/queries/get_resident_relatives_query.dart';
 import 'application/queries/get_resident_summary_query.dart';
 import 'application/queries/get_resident_list_query.dart';
-import 'presentation/bloc/nursing_home_setup_bloc.dart';
 import 'presentation/bloc/nursing_bloc.dart';
 
 void initNursingModule(GetIt locator) {
@@ -20,24 +15,15 @@ void initNursingModule(GetIt locator) {
   locator.registerLazySingleton<NursingRemoteDataSource>(
     () => NursingRemoteDataSourceImpl(client: locator()),
   );
-  locator.registerLazySingleton<NursingHomeSetupRemoteDataSource>(
-    () => NursingHomeSetupRemoteDataSourceImpl(client: locator()),
-  );
-
   // 2. Repositories
   locator.registerLazySingleton<INursingRepository>(
     () => NursingRepositoryImpl(remoteDataSource: locator()),
   );
-  locator.registerLazySingleton<INursingHomeSetupRepository>(
-    () => NursingHomeSetupRepositoryImpl(remoteDataSource: locator()),
-  );
-
   // 3. Use Cases / Queries
   locator.registerLazySingleton(() => GetResidentSummaryQuery(locator()));
   locator.registerLazySingleton(() => GetResidentListQuery(locator()));
   locator.registerLazySingleton(() => GetFamilyUsersQuery(locator()));
   locator.registerLazySingleton(() => GetResidentRelativesQuery(locator()));
-  locator.registerLazySingleton(() => CreateNursingHomeCommand(locator()));
   locator.registerLazySingleton(() => CreateResidentCommand(locator()));
   locator.registerLazySingleton(() => CreateRelativeCommand(locator()));
 
@@ -53,5 +39,4 @@ void initNursingModule(GetIt locator) {
       locator(),
     ),
   );
-  locator.registerFactory(() => NursingHomeSetupBloc(locator()));
 }
